3f6722ce224533670f2860dc1a464c554e7672d0
Capybara52 <138335483+Capybara52@users.noreply.github.com>
2026-06-24 21:04:40 -0500

    WISP

--- Milton/SCRIPT/wisp.gd
+++ Milton/SCRIPT/wisp.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-@onready var target=$"../PlayerPh"
+@onready var target=$"../../Player"
 var speed=150
 func _physics_process(_delta):
 		var direction =(target.position-position).normalized()
 		velocity=direction * speed
 		look_at(target.position)
 		move_and_slide()
+		pass
