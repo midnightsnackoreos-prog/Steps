1cd9424abe3e776c92a22f26e10ed6fbd4e4c904
midnightsnackoreos-prog <midnightsnackoreos@gmail.com>
2026-06-25 16:52:08 +0900

    renaming scripts and trying to make shrine script

---
+++ area_2d.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@onready var panel = $"../CanvasLayer/Panel"
+@onready var dialogue = $"../CanvasLayer/Panel/DialogueLabel"
+@onready var interact_label = $InteractLabel
+
+
+var player_near = false
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_near = true
+		interact_label.visible = true
+		dialogue.visible = true
+		
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_near = false
+		interact_label.visible = false
+		dialogue.visible = false
+		panel.visible = false
+		
+		
+func _process(_delta):
+	if player_near and Input.is_action_just_pressed("interact"):
+		if panel.visible:
+			panel.visible = false
+			interact_label.visible = true
+		else: 
+			panel.visible = true
+			dialogue.text = ("You have regained 25 energy.")
+			interact_label.visible = false
