4ffe59b9b88c14addfcb4b41ad165a4d65a9bc96
midnightsnackoreos-prog <midnightsnackoreos@gmail.com>
2026-06-29 10:39:50 +0900

    vending machine creataed
    script works, press e to interact, have not implemented coin restriction yet.

--- Ryan/scripts/vending_machine.gd
+++ Ryan/scripts/vending_machine.gd
@@ -1,1 +1,42 @@
 extends Area2D
+
+@onready var dialogue = $"../CanvasLayer/Panel/DialogueLabel"
+@onready var interact_label = $InteractLabel
+@onready var panel = $"../CanvasLayer/Panel"
+
+
+var used = false
+var player_near = false
+var player = null
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player = body
+		player_near = true
+		interact_label.visible = true
+		dialogue.visible = true
+		
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player = null
+		player_near = false
+		interact_label.visible = false
+		dialogue.visible = false
+		panel.visible = false
+		
+		
+func _process(_delta):
+	if player_near and Input.is_action_just_pressed("interact"):
+		if panel.visible:
+			panel.visible = false
+			interact_label.visible = true
+			if player:
+				player.can_move = true
+
+		else: 
+			panel.visible = true
+			dialogue.text = ("25 Energy Restored")
+			interact_label.visible = false
+			if player:
+				player.can_move = false
