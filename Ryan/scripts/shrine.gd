9155ea69322a67503ce340dee6bae7647efcde0a
midnightsnackoreos-prog <midnightsnackoreos@gmail.com>
2026-06-29 19:07:00 +0900

    shrine teleport complete
    press e to restore energy, press e again to teleport

--- Ryan/scripts/shrine.gd
+++ Ryan/scripts/shrine.gd
@@ -3,12 +3,14 @@ extends Area2D
 @onready var panel = $"../CanvasLayer/Panel"
 @onready var dialogue = $"../CanvasLayer/Panel/DialogueLabel"
 @onready var interact_label = $InteractLabel
-
+@onready var teleport_marker = $"../../SecondArea"
 
 
 var used = false
 var player_near = false
 var player = null
+var waiting_to_teleport = false
+
 
 func _on_body_entered(body):
 	if body.name == "Player":
@@ -29,26 +31,22 @@ func _on_body_exited(body):
 		
 func _process(_delta):
 	if player_near and Input.is_action_just_pressed("interact"):
-		if panel.visible:
+		if waiting_to_teleport:
+			waiting_to_teleport = false
+			
 			panel.visible = false
-			interact_label.visible = true
+			interact_label.text = "[E] Restore Energy"
 			if player:
-				player.can_move = true
+				player.global_position = teleport_marker.global_position
+			return
 
-		else: 
+
+		if !panel.visible:
 			panel.visible = true
-			dialogue.text = ("Good luck, traveler.")
-			interact_label.visible = false
-			if player:
-				player.can_move = false
+			dialogue.text = ("Your energy has been restored.\n Press [E] to continue your journey.")
+			interact_label.text = "[E] Continue"
+		if player:
+			player.can_move = false
 				
+		waiting_to_teleport = true
 			
-			
-func _activate(player):
-	if used:
-		return
-	used = true
-	print("Shrine activated")
-	
-func do_effect(player):
-	player.energy = player.max_energy
