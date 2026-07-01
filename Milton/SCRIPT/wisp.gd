93889922668109b1b235441674f3f1812b5281a4
RoadRunnerDevil <tristaniarzu@gmail.com>
2026-06-30 23:31:25 -0700

    knockback

--- Milton/SCRIPT/wisp.gd
+++ Milton/SCRIPT/wisp.gd
@@ -4,7 +4,75 @@ extends CharacterBody2D
 @export var target_to_chase: CharacterBody2D
 const SPEED = 180
 
+<<<<<<< Updated upstream
 func _physics_process(delta: float) -> void:
 	navigation_agent.target_position = target_to_chase.global_position
 	velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * SPEED
 	move_and_slide()
+=======
+
+var speed = 30
+var player_chase = false
+var player = null
+var health: int = 100
+var max_health := 100
+var knockback=Vector2.ZERO
+
+func _ready():
+	$healthbar.visible = false
+	detection_icon.visible = false
+
+
+func _physics_process(delta):
+	global_position += knockback * delta
+	knockback = knockback.move_toward(Vector2.ZERO, 1000 * delta)
+	
+	if player_chase:
+		global_position += (player.global_position - global_position) / speed
+		$AnimatedSprite2D.play("CHASE")
+	else:
+		$AnimatedSprite2D.play("slumber")
+		
+
+
+
+
+func _on_detection_area_body_entered(body):
+	if body.name != "Player":
+		return
+		
+	player = body
+	player_chase = true
+	
+	detection_icon.visible = true
+	detection_timer.start()
+	
+
+
+func _on_detection_area_body_exited(body):
+	
+	player = null
+	player_chase = false
+
+
+func _on_detection_timer_timeout() -> void:
+	detection_icon.visible = false
+
+# DAMAGE AND HEALTH---------------------
+	
+func take_damage(damage : int, knockback_force: Vector2) -> void:
+	$healthbar.visible = true
+	health -= damage
+	health = max(0, health)
+	print(health)
+	if health <= 0:
+		die()
+
+	knockback=knockback_force
+
+
+func die():
+	queue_free()
+
+	
+>>>>>>> Stashed changes
