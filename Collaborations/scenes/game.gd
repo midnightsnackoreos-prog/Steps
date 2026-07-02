96190634ad824ec36c9eb94a08e53a8fbee30c06
midnightsnackoreos-prog <midnightsnackoreos@gmail.com>
2026-07-02 14:32:55 +0900

    get rid of energy bar and label when dead

--- Collaborations/scenes/game.gd
+++ Collaborations/scenes/game.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var energy_manager = $EnergyManager
 @onready var animation_player = $GlobalUI/AnimationPlayer
 @onready var death_label = $GlobalUI/DeathLabel
+@onready var energy_bar = $GlobalUI/CanvasLayer/EnergyBar
+@onready var energy_label = $GlobalUI/CanvasLayer/EnergyLabel
 
 
 func _ready():
@@ -15,8 +17,12 @@ func _on_player_died():
 	
 	animation_player.play("death")
 	death_label.visible = true
+	energy_bar.visible = false
+	energy_label.visible = false
 	await animation_player.animation_finished
 	death_label.visible = false
+	energy_bar.visible = true
+	energy_label.visible = true
 	
 	if energy_manager.checkpoint:
 		player.global_position = energy_manager.checkpoint.global_position
